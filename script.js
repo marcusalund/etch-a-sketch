b8a4eb69e395b5255610bbc95d8afa36436bb6f7
Marcus Lund <marcusalund@gmail.com>
2022-06-03 14:09:02 -0600

    Complete steps 3-6 in TOP

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gridContainer = document.querySelector(".grid-container");
-let defaultGridSize = 32;
+let defaultGridSize = 8;
 
 function setupGridContainer(size){
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -8,10 +8,31 @@ function setupGridContainer(size){
     for(let i = 0; i < size * size; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add('grid-element');
-        gridElement.addEventListener('mouseover', () => {gridElement.style.backgroundColor = 'black'});
+        gridElement.addEventListener('mouseover', () => {gridElement.style.backgroundColor = randomHex()});
         gridContainer.appendChild(gridElement);
     }
 }
+
+function resetGridContainer(size) {
+    gridContainer.innerHTML = '';
+    setupGridContainer(size);
+}
+
+function randomHex(){
+    let randomColor = '';
+
+    let r = 0;
+    let g = Math.floor(Math.random()*256);
+    let b = 0;
+
+    randomColor = `rgb(${r}, ${g}, ${b})`;
+
+    return randomColor;
+}
+
+const slider = document.querySelector('.slider');
+
+slider.onchange = (e) => resetGridContainer(e.target.value);
 
 
 window.onload = () => {
