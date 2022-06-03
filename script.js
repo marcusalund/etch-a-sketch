0cd4254bee1452f5d09bc3561aaf2c4f6cd6d738
Marcus Lund <marcusalund@gmail.com>
2022-06-03 13:36:42 -0600

    Setup grid and add event listener for mouseover

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Hello")
+const gridContainer = document.querySelector(".grid-container");
+let defaultGridSize = 32;
+
+function setupGridContainer(size){
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridRowColumns = `repeat(${size}, 1fr)`;
+
+    for(let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', () => {gridElement.style.backgroundColor = 'black'});
+        gridContainer.appendChild(gridElement);
+    }
+}
+
+
+window.onload = () => {
+    setupGridContainer(defaultGridSize);
+}
